d0661ebba8133ee4dda5c0acfc6ef10f26a460aa
Samuel Kerboeuf <samuel.kerboeuf@gmail.com>
2013-04-08 23:44:01 +0200

    Fixing bugs with dashboard

--- js/dropdown.js
+++ js/dropdown.js
@@ -2,13 +2,14 @@ var Dropdown =
 {
     init: function()
     {
-        $( '.dropdown' ).on( 'click', Dropdown.show );
+        $( '#session .dropdown' ).on( 'click', Dropdown.show );
+        $( '#dashboard .dropdown' ).on( 'click', Dropdown.dashboard );
     },
     show: function( event )
     {
         event.stopPropagation();
         
-        $( '.dropdown' ).removeClass( 'open' );
+        $( '#session .dropdown' ).removeClass( 'open' );
         $( this ).addClass( 'open' );
         
         $( 'body' ).one( 'click', Dropdown.hide );
@@ -17,6 +18,27 @@ var Dropdown =
     {
         event.stopPropagation();
         
-        $( '.dropdown' ).removeClass( 'open' );
+        $( '#session .dropdown' ).removeClass( 'open' );
+    },
+    dashboard: function( event )
+    {
+        event.stopPropagation();
+        
+        var level = $( this ).attr('data-level');
+        
+        switch( level )
+        {
+            case '1':
+                $('.dropdown[data-level=1], .dropdown[data-level=2], .dropdown[data-level=3]').removeClass( 'open' );
+                break;
+            case '2':
+                $('.dropdown[data-level=2], .dropdown[data-level=3]').removeClass( 'open' );
+                break;
+            case '3':
+                $('.dropdown[data-level=3]').removeClass( 'open' );
+                break;
+        }
+        
+        $( this ).addClass( 'open' );
     }
 };
