b775f1b01ae03bae5e1365762ba63ce22301baf6
Samuel Kerboeuf <samuel.kerboeuf@gmail.com>
2013-04-13 13:02:12 +0200

    Adding affinity bot mouseover & mouseout

--- js/affinity-bot.js
+++ js/affinity-bot.js
@@ -1,8 +1,13 @@
 var AffinityBot =
 {
+    affinityArc: [],
+    affinityBg: [],
     init: function()
     {
         $('.affinity-bot').each( AffinityBot.set );
+        
+        $('.affinity-info').on( 'mouseover', AffinityBot.onHover );
+        $('.affinity-info .affinity-actions').on( 'mouseleave', AffinityBot.onOut );
     },
     set: function()
     {
@@ -10,6 +15,15 @@ var AffinityBot =
         var score = parseInt( $( this ).attr('data-score') );
         var size = parseInt( $( this ).attr('data-size') );
         var stroke = parseInt( $( this ).attr('data-stroke') );
+        
+        var id = parseInt( $( this ).attr('data-id') );
+        
+        if ( !id )
+            id = Math.floor( Math.random() );
+        
+        var colorStroke = "#f68b2c";
+        
+        var colorFill = "#fdcca1";
         
         // Set affinity bot size
         var fullSize = size + stroke;
@@ -21,17 +35,80 @@ var AffinityBot =
 
         affinityType.customAttributes.arc = AffinityBot.arc;
         
-        var affinityArc = affinityType.path().attr( {"fill": "none", "stroke": "#f68b2c", "stroke-width": stroke, arc: [radius, radius, 0, 100, midSize]} );
+        AffinityBot.affinityArc[id] = affinityType.path().attr( {"fill": "none", "stroke": colorStroke, "stroke-width": stroke, arc: [radius, radius, 0, 100, midSize]} );
 
         // Animate affinity boat circle
         var anim = Raphael.animation(  {arc: [radius, radius, score, 100, midSize]}, 1000, "linear" );
 
-        affinityArc.animate( anim.delay( 500 ) ); 
+        AffinityBot.affinityArc[id].animate( anim.delay( 500 ) ); 
 
         // Create background circle
         var paper = Raphael( $( this )[0], fullSize, fullSize );
-        var c = paper.circle(radius, radius, midSize);
-        c.attr( {stroke: "#fdcca1", "stroke-width": stroke} );
+        AffinityBot.affinityBg[id] = paper.circle(radius, radius, midSize);
+        AffinityBot.affinityBg[id].attr( {stroke: colorFill, "stroke-width": stroke} );
+    },
+    onHover: function()
+    {
+        var id = parseInt( $( '.affinity-bot', this ).attr('data-id') );
+        
+        if ( id )
+        {
+            $( this ).addClass( 'active' );
+            
+            // Retrieve raw data
+            var score = parseInt( $( '.affinity-bot', this ).attr('data-score') );
+            var size = parseInt( $( '.affinity-bot', this ).attr('data-size') );
+            var stroke = parseInt( $( '.affinity-bot', this ).attr('data-stroke') );
+
+            var colorStroke = "#00a9da";
+
+            var colorFill = "#ccebf4";
+
+            // Set affinity bot size
+            var fullSize = size + stroke;
+            var midSize = size / 2;
+            var radius = parseInt( fullSize / 2 );
+
+            AffinityBot.affinityArc[id].attr( {"fill": "none", "stroke": colorStroke, "stroke-width": stroke, arc: [radius, radius, 0, 100, midSize]} );
+
+            // Animate affinity boat circle
+            var anim = Raphael.animation(  {arc: [radius, radius, score, 100, midSize]}, 1000, "linear" );
+
+            AffinityBot.affinityArc[id].animate( anim.delay( 300 ) );
+            AffinityBot.affinityBg[id].animate({stroke: colorFill}, 300);
+        }
+    },
+    onOut: function()
+    {   
+        var parent = $( this ).parents( '.affinity-info' )[ 0 ];
+        
+        var id = parseInt( $( '.affinity-bot', parent ).attr('data-id') );
+        
+        if ( id )
+        {
+            $( parent ).removeClass( 'active' );
+            
+            // Retrieve raw data
+            var score = parseInt( $( '.affinity-bot', parent ).attr('data-score') );
+            var size = parseInt( $( '.affinity-bot', parent ).attr('data-size') );
+            var stroke = parseInt( $( '.affinity-bot', parent ).attr('data-stroke') );
+
+            var colorStroke = "#f68b2c";
+            var colorFill = "#fdcca1";
+
+            // Set affinity bot size
+            var fullSize = size + stroke;
+            var midSize = size / 2;
+            var radius = parseInt( fullSize / 2 );
+
+            AffinityBot.affinityArc[id].attr( {"fill": "none", "stroke": colorStroke, "stroke-width": stroke, arc: [radius, radius, 0, 100, midSize]} );
+
+            // Animate affinity boat circle
+            var anim = Raphael.animation(  {arc: [radius, radius, score, 100, midSize]}, 1000, "linear" );
+
+            AffinityBot.affinityArc[id].animate( anim.delay( 300 ) );
+            AffinityBot.affinityBg[id].animate({stroke: colorFill}, 300);
+        }
     },
     arc: function(xloc, yloc, value, total, R)
     {
