7c46a1bb7d8cc275ef75786a143434114b9b18b8
Samuel Kerboeuf <samuel.kerboeuf@gmail.com>
2013-04-14 15:29:04 +0200

    Adding expert advice - quizz page

--- js/modal.js
+++ js/modal.js
@@ -3,6 +3,17 @@ var Modal =
     init: function()
     {
         $( '.close-modal-background-target, .modal-close' ).on( 'click', Modal.hide );
+        
+        $( ".advice-expert" ).accordion( { header: "> div > h3", icons: null } )
+        .sortable(
+        {
+            axis: "y",
+            handle: "h3",
+            stop: function( event, ui )
+            {
+                ui.item.children( "h3" ).triggerHandler( "focusout" );
+            }
+        });
     },
     show: function()
     {
