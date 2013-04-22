afd62f7910f39ba1033055d2752a762ae1852fa7
Christophe Valentin <christophe.valentin@wepingo.com>
2013-04-22 15:59:32 +0200

    replace search-bar by link in header

--- js/dropdown.js
+++ js/dropdown.js
@@ -9,8 +9,36 @@ var Dropdown =
         
         $( '.geoloc-products .dropdown' ).bind( 'click', Dropdown.geolocMap );
         
+		// insta search behaviour
         $( '#search input').focus( Dropdown.searchShow );
-        $( '#search input').focusout( Dropdown.searchHide );
+       	$( '#search input').focusout( Dropdown.searchHide );
+
+		// small search behaviour
+		$('#small-search a').on('click',Dropdown.search);
+    },
+	search: function()
+    {    		   
+    	$('#small-search a').addClass('notvisible');
+    	$('#search-container').css('position','relative'); 
+	    $('#search-container form').addClass('visible');
+	    
+	    //RESIZE
+	    if ( $(window).width() <= 1107){
+    		$('#session').addClass('notvisible');
+        }
+        
+        // BODY CLICK
+	    $('#search-container form').click(function(event) {
+		    $('html').one('click',function() {
+			    $('#small-search a').removeClass('notvisible');
+			    $('#session').removeClass('notvisible');
+			    $('#search-container').css('position','absolute');
+			    $('#search-container form').removeClass('visible');
+			});
+		    event.stopPropagation();
+		});
+
+
     },
     searchShow: function ( event )
     {
